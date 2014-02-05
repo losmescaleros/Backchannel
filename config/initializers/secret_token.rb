170d2c677b359b66b56b5436238a2fd50fb65e09
losmescaleros <losmescaleros@gmail.com>
2014-02-04 19:49:09 -0500

    Initial check-in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Backchannel::Application.config.secret_token = '001a94be5aa2e387e4df537f611a529ad89096fed6538078ed49623d57c3523e6a6aac9fe978f55bb9feb0d5c27309e59da27d11743f8d58fa9fe299e5639121'
