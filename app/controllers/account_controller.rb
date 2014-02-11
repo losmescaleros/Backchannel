8f3c801c032ca766e37df2aa5f9bd4de3f6c13f0
Will <wmorrow@winterhoof>
2014-02-11 14:40:04 -0500

    Worked on account_controller, user Not sure if this is even the right way to do things

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,10 +1,47 @@
 class AccountController < ApplicationController
   def register
+    @user = User.new
   end
 
   def login
+    puts login_params
+    @user = User.authenticate(login_params)
+    if @user != nil
+      reset_session
+      session[:email] = @user.email
+      session[:name] = @user.name
+      session[:user_level] = @user.user_level
+      format.html { redirect_to @index, notice: 'Successfully logged in!' } #I'm honestly not sure how these redirect_to's work but the intent should be clear
+      format.json { render action: 'show', status: :logged_in, location: @index }
+    end
   end
 
   def logout
+    reset_session
+    format.html { redirect_to @index, notice: 'Successfully logged out!' }
+    format.json { render action: 'show', status: :logged_out, location: @index }
+  end
+
+  def create
+    puts user_params
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @login, notice: 'Successfully registered!' }
+        format.json { render action: 'show', status: :created, location: @login }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def user_params
+    params.require(:email).require(:name).require(:password)
+  end
+
+  def login_params
+    params.require(:email).require(:password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :user_level
+  after_initialize :default_values
+
   validates :email, presence: true,
             uniqueness: true
   validates :name, presence: true
@@ -8,4 +10,12 @@ class User < ActiveRecord::Base
 
   has_many :posts
 
+  def authenticate(email, password)
+    where(email: email, password: password).first
+  end
+
+  private
+  def default_values
+    self.user_level ||= 0
+  end
 end
